2da9386e73908696b89375679dd6ffd99633527b
harry-26 <85989001+harry-26@users.noreply.github.com>
2021-09-17 15:33:45 +0800

    exe

--- resources/views/ExecutiveOrder2021.blade.php
+++ resources/views/ExecutiveOrder2021.blade.php
@@ -26,6 +26,80 @@
         </div>
     </div> --}}
     <style>
+    @media only screen and (max-width: 1449px) and (min-width: 1201px){
+      #executive img.card-img-top {
+          height: 100%!important;
+      }
+      #executive img.card-img-top{
+        height: 100% !important;
+      }
+      #executive .card-body h4{
+        font-size: 18px !important;
+        font-weight: bold;
+      }
+      #executive .card-body button{
+        font-size: 15px !important;
+        padding-top: 5%;
+        padding-bottom: 5%;
+        padding-left: 5%;
+        padding-right: 5%;
+      }
+    }
+    @media only screen and (max-width: 1920px) and (min-width: 1601px){
+      #executive{
+        max-width: 100% !important;
+        width: 100% !important;
+        flex: 0 0 100%;
+      }
+      #executive img.card-img-top{
+        height: 100% !important;
+      }
+      #executive .card-body h4{
+        font-size: 18px !important;
+        font-weight: 600;
+      }
+      #executive .card-body button{
+        font-size: 15px !important;
+        padding-top: 5%;
+        padding-bottom: 5%;
+        padding-left: 5%;
+        padding-right: 5%;
+      }
+      #executive .card-body .btnv{
+        font-size: 15px !important;
+        padding-top: 5%;
+        padding-bottom: 5%;
+        padding-left: 12%;
+        padding-right: 12%;
+      }
+
+    }
+    @media screen and (min-device-width: 1205px) and (max-device-width: 1366px){
+      #executive{
+        max-width: 100% !important;
+        width: 100% !important;
+        flex: 0 0 100%;
+      }
+      #executive img.card-img-top{
+        height: 100% !important;
+      }
+      #executive .card-body h4{
+        font-size: 15px !important;
+      }
+      #executive .card-body button{
+        font-size: 10px !important;
+        padding-top: 3%;
+        padding-bottom: 3%;
+        padding-left: 3%;
+        padding-right: 3%;
+      }
+      .navbar-nav li {
+          list-style: none;
+          display: inline-block;
+          margin: 0px -4px;
+          position: relative;
+      }
+    }
     @media screen and (max-width: 766px) and (min-width: 200px) {
     #executive .modalss{
       width: 100% !important;
@@ -34,27 +108,31 @@
     #executive .exebanner h1{
       font-size: 15px !important;
     }
+    #executive .card-body button{
+      font-size: 15px !important;
+      padding-top: 3%;
+      padding-bottom: 3%;
+      padding-left: 3%;
+      padding-right: 3%;
     }
-    @media screen and (max-width: 1366px) and (min-width: 200px) {
-    #mac{
-    height: 1300px !important;
+    #executive .card-body h4{
+      font-size: 15px !important;
     }
-    }
-
+  }
     </style>
     <div class="container 1st_cont" id="executive" style="padding-right: 0 !important; padding-left: 0 !important;">
           <div class="modal fade" id="fmz091" data-keyboard="false" data-backdrop="static">
             <div class="modal-dialog modal-lg modalss" style="width: 90%; max-width: 90%;">
 
-              <!-- Modal content-->
+
               <div class="modal-content">
                 <div class="modal-body">
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091a.png')}}"/>
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091b.png')}}"/>
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091c.png')}}"/>
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091d.png')}}"/>
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091e.png')}}"/>
-                    <img class="card-img-top mac" src="{{asset('assets/transparency/executiveorder2021/fmz091f.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091a.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091b.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091c.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091d.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091e.png')}}"/>
+                    <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/fmz091f.png')}}"/>
                 </div>
                 <div class="modal-footer">
                   <button type="button" class="btn btn-default close_pause"  data-dismiss="modal" style="background-color:red;">Close</button>
@@ -69,21 +147,25 @@
               <h1 style="position: absolute; top: 50%; left: 35%;transform: translate(-50%, -50%); color:white !important; font-size:50px; text-shadow: 1px 1px 2px black, 0 0 1em blue, 0 0 0.2em darkblue;">EXECUTIVE ORDER SERIES OF 2021</h1>
               <img style=" width: 100%;" src="{{asset('assets/bandept2.png')}}" />
             </div>
-              <div class="row mt-5" style="margin-bottom: 2%;">
+          </div>
+              <div class="row mt-3" style="margin-bottom: 2%;">
                   <div class="col-md-3 clearfix d-md-block" style="margin:auto;">
+                    <a data-toggle="modal" data-target="#fmz091" >
                     <div class="card mb-2">
-                      <img class="card-img-top" src="{{asset('assets/transparency/Bayanihan Grant 1 of 2.jpg')}}"
+                      <img class="card-img-top" src="{{asset('assets/transparency/executiveorder2021/coverfmz091.png')}}"
                            alt="Card image cap">
                       <div class="card-body">
                         <h4>EXECUTIVE ORDER NO. FMZ-091 SERIES OF 2021</h4>
-                        <br>
-                        <button type="button" class="btn btn-info btn-lg" id="optic_btn" data-toggle="modal" data-target="#fmz091" style="float: right; font-size:18px;" >VIEW</button>
+                        <button type="button" class="btn btn-info btn-lg btnv" id="optic_btn" data-toggle="modal" data-target="#fmz091" style=" float: right; font-size:18px;" >VIEW</button>
+                        <a href="{{asset('assets/transparency/executiveorder2021/executiveordernofmz-091.pdf')}}" download>
+                          <button type="button" class="btn btn-info btn-lg" id="optic_btn" style=" float: right; font-size:18px;" >DOWNLOAD</button>
+                        </a>
                         {{-- <img class="card-img-top" src="{{asset('assets/play-button.svg')}}" style="width: 5%;" > --}}
                       </div>
                     </div>
+                    </a>
                   </div>
             </div>
         </div>
-    </div>
 
 @endsection
