f12581ba4a0359f3497229196f8a7c7591bc4b4f
harry-26 <85989001+harry-26@users.noreply.github.com>
2022-01-13 13:05:03 +0800

    Update nav-bar.blade.php

--- resources/views/layouts/nav-bar.blade.php
+++ resources/views/layouts/nav-bar.blade.php
@@ -40,7 +40,7 @@
           <a href="{{route('calendar')}}"><img  style="width:13%; position: fixed; margin-top: 28%; z-index: 999; margin-left: 85%;"src="{{asset('assets/yorme.png')}}"/></a>
         </div> -->
         <button onclick="topFunction()" id="myBtn" title="Go to top"><i class="fa fa-arrow-up" style="font-size:30px;"></i></button>
-        <div class="container snowflakes" id="nav">
+        <!-- <div class="container snowflakes" id="nav">
           <div class="snowflake snowsnow">
            ❅
            </div>
@@ -77,7 +77,7 @@
            <div class="snowflake snowsnow">
              ❆
            </div>
-         </div>
+         </div> -->
         <div class="container" id="nav">
             <div class="row" style="background:rgb(27, 37, 96); color:#fff; padding: 4px 0;">
               {{-- <div class="soc_med col-sm-6">
