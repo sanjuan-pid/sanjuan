3f5304f369d9ccfadc172e48eb1448e173e6afce
harry-26 <85989001+harry-26@users.noreply.github.com>
2022-01-13 16:03:18 +0800

    contentsnews

--- resources/views/news/contents.blade.php
+++ resources/views/news/contents.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.nav-bar')
 @section('content')
-<div class="container" id="news_contents">
-    <div class="row" id="news_contents_row">
+<div class="container" id="news_contents" style="width:90%;">
+    <div class="row d-flex justify-content-center" id="news_contents_row">
         <div class="col-sm-9" >
             <br><br>
             <div class="card">
@@ -9,27 +9,26 @@
                         <br>
                         @foreach($ann as $row)
                             <img src="{{asset("uploads/" . $row->filename) }}"  style="width: 100%; padding: 0 25%;  margin-bottom: 5%;" />
-                            <h1>{{ $row->title }}</h1>
-                            <br>
+                            <h1 style="font-size:30px !important;">{{ $row->title }}</h1>
                             <p><?php echo $row->desc ?></p>
-                            <br>
                         @endforeach
-                        
+
                 </div>
             </div>
             <br><br>
         </div>
         <div class="col-sm-3"style="margin-bottom: 7%;" >
             <br><br>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 25rem;">
                 <div class="card-header">
-                    Latest News
+                    <h6 class="titlenews">Latest News</h6>
                 </div>
                 <ul class="list-group list-group-flush">
                     <?php $i=0; ?>
                     @foreach($news as $row)
+                        <img src="{{asset("uploads/" . $row->filename) }}"  style="width: 100%; padding: 0 25%;  margin-bottom: 5%;" />
                         <?php $i++; ?>
-                        <li class="list-group-item latest_news"><a  href="{{action( 'Pages@news_contents', $row['id'] )}}"  > {{$row['title']}} </a></li>
+                        <li class="list-group-item latest_news" ><a style="color:black;" href="{{action( 'Pages@news_contents', $row['id'] )}}"  > {{$row['title']}} </a></li>
                         @if ($i == 3)
                             @break
                         @endif
@@ -37,23 +36,24 @@
                 </ul>
             </div>
             <br><br>
-            <div class="card" style="width: 18rem;">
+            <div class="card" style="width: 25rem;">
                 <div class="card-header">
-                    Current Events
+                    <h6 class="titlenews">Current Events</h6>
                 </div>
                 <ul class="list-group list-group-flush">
                     <?php $i=0; ?>
                     @foreach($events as $row)
+                      <img src="{{asset("uploads/" . $row->filename) }}"  style="width: 100%; padding: 0 25%;  margin-bottom: 5%;" />
                         <?php $i++; ?>
-                        <li class="list-group-item latest_news"><a  href="{{action( 'Pages@news_contents', $row['id'] )}}"  > {{$row['title']}} </a></li>
+                        <li class="list-group-item latest_news"><a  style="color:black;" href="{{action( 'Pages@news_contents', $row['id'] )}}"  > {{$row['title']}} </a></li>
                         @if ($i == 3)
                             @break
                         @endif
                     @endforeach
                 </ul>
-            </div>            
+            </div>
         </div>
     </div>
-</div>   
+</div>
 
 @endsection
