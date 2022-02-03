5cbda42c9c2f634b095f5521f890fd1fb06c81a7
josephriverajr <83931408+rlyngnzls@users.noreply.github.com>
2022-02-03 16:01:35 +0800

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -62,6 +62,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'preventBackHistory' => \App\Http\Middleware\PreventBackHistory::class,
+        'is_admin' => \App\Http\Middleware\IsAdmin::class,
     ];
 
     /**
