d201cd1f6b9e716fb948451e00cbd774397ff801
josephriverajr <sanjuanwebsitepid2021@gmail.com>
2022-02-11 11:03:50 +0800

    Update AnnouncementController.php

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -12,7 +12,7 @@ class AnnouncementController extends Controller
 {
     public function __construct()
     {
-        $this->middleware('auth:admin');
+        $this->middleware('auth');
     }
 
     /**
