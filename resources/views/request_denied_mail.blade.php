8f52815b386b6d1d8fa13cce8f9379138344b134
sjwebsite <89233665+sjwebsite@users.noreply.github.com>
2022-02-16 14:12:57 +0800

    Update request_denied_mail.blade.php

--- resources/views/request_denied_mail.blade.php
+++ resources/views/request_denied_mail.blade.php
@@ -6,11 +6,11 @@
 <body>
     <p>Good day, {{$data['last_name']}} {{$data['suffix']}}, {{$data['first_name']}} {{$data['middle_name']}}</p>
 
-    <p>We have received the request for your Covid19 Certificate of Recovery. Unfortunately your request have been declined for the following reason(s):</p>
+    <p>We have received the request for your Covid19 Certificate of Recovery. Unfortunately, your request has declined for the following reason(s);</p>
 
     <p>{!! nl2br($data['remarks']) !!}</p>
 
-    <p>You may go to our office located near Landbank San Juan to submit any requirements stated in the remarks.</p>
+    <p>Please visit our office at GF, CDRRMO Office, Steel Parking, Dr. P. A. Narciso St. Corazon De Jesus, beside Landbank San Juan. Office hours are from Monday to Friday from 8 AM to 5 PM. Bring your documents if needed or stated in remarks.</p>
 
     <p>Thank you for your understanding.</p>
 
