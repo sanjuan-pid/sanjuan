8508e06007c76f3f116d8d0e9aad7f7b85bfe13f
harry-26 <85989001+harry-26@users.noreply.github.com>
2022-03-08 14:46:51 +0800

    Update contents.blade.php

--- resources/views/news/contents.blade.php
+++ resources/views/news/contents.blade.php
@@ -1,5 +1,27 @@
 @extends('layouts.nav-bar')
 @section('content')
+<style>
+.badge, .btn, .card:not([class*=card-outline-]), .chip, .jumbotron, .modal-dialog.cascading-modal .modal-c-tabs .nav-tabs, .modal-dialog.modal-notify .modal-header, .navbar, .pagination .active .page-link, .z-depth-1 {
+    -webkit-box-shadow: 0 2px 5px 0 rgb(0 0 0 / 16%), 0 2px 10px 0 rgb(0 0 0 / 12%);
+    box-shadow: 0 2px 5px 0 rgb(0 0 0 / 0%), 0 2px 10px 0 rgb(0 0 0 / 0%) !important;
+}
+.titlenews {
+    color: #1b2560 !important;
+    font-weight: 700;
+    font-size: 22px;
+    border-bottom: 5px solid #1b2560;
+    line-height: 48px;
+}
+.card-header {
+    padding: 0.75rem 1.25rem;
+    margin-bottom: 0;
+    background-color: rgb(253 253 253) !important;
+    border-bottom: 0px solid rgba(0,0,0,.125) !important;
+}
+.list-group-flush>.list-group-item {
+    border-width: 0 0 0px;
+}
+</style>
 <div class="container" id="news_contents" style="width:90%;">
     <div class="row d-flex justify-content-center" id="news_contents_row">
         <div class="col-sm-9" >
@@ -17,9 +39,8 @@
             </div>
             <br><br>
         </div>
-        <div class="col-sm-3"style="margin-bottom: 7%;" >
-            <br><br>
-            <div class="card" style="width: 25rem;">
+        <div class="col-sm-3 mt-5"style="margin-bottom: 7%; padding:0;" >
+            <div class="card" style="width: 20rem;">
                 <div class="card-header">
                     <h6 class="titlenews">Latest News</h6>
                 </div>
@@ -35,8 +56,7 @@
                     @endforeach
                 </ul>
             </div>
-            <br><br>
-            <div class="card" style="width: 25rem;">
+            <div class="card mt-5" style="width: 20rem;">
                 <div class="card-header">
                     <h6 class="titlenews">Current Events</h6>
                 </div>
